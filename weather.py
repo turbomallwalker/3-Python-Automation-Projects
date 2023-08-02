18c7f6fc58801226e6ad59517cea13198204ec3f
Jeremy <106548828+turbomallwalker@users.noreply.github.com>
2023-08-02 18:35:50 -0500

    Update weather.py
    Conversion for Fahrenheit

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "f493235022f117443717201d55ff1d93"
+API_KEY = "********************" #insert your own api key here. 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
@@ -10,9 +10,12 @@ response = requests.get(request_url)
 if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
-    temperature = round(data["main"]["temp"] - 273.15, 2)
+    temperature_kelvin = data["main"]["temp"]
+    
+    # Convert the temperature from Kelvin to Fahrenheit
+    temperature_fahrenheit = round((temperature_kelvin - 273.15) * 9/5 + 32, 2)
 
     print("Weather:", weather)
-    print("Temperature:", temperature, "celsius")
+    print("Temperature:", temperature_fahrenheit, "Fahrenheit")
 else:
     print("An error occurred.")
